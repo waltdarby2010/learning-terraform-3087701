324c680e97aabb50934a31442aecf9aa18172387
waltdarby2010 <75259490+waltdarby2010@users.noreply.github.com>
2023-04-17 13:05:10 -0400

    create sec group

--- main.tf
+++ main.tf
@@ -30,6 +30,7 @@ resource "aws_instance" "blog" {
 
 resource "aws_security_group" "blog" {
   name = "blog"
+  description = "Allow HTTP and HTTPS in. Allow everything out."
   tags = {
     Terraform = "true"
   }
