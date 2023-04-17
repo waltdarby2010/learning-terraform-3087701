d6b303a13a93184cc2d43df273f85a388da111a9
waltdarby2010 <75259490+waltdarby2010@users.noreply.github.com>
2023-04-17 13:41:51 -0400

    use modeuly for sec group

--- main.tf
+++ main.tf
@@ -30,12 +30,16 @@ resource "aws_instance" "blog" {
 
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
-  version = "4.13.0"
+  version = "4.17.2"
+  name = "blog_new"
 
   vpc_id  = data.aws_vpc.default.id
-  name    = "blog"
-  ingress_rules = ["https-443-tcp","http-80-tcp"]
+  
+  ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
-  egress_rules = ["all-all"]
-  egress_cidr_blocks = ["0.0.0.0/0"]
+
+  egress_rules       = ["all-all"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+
 }
+
