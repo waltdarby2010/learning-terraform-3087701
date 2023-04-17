fd780b9a071c1e0f1cd552b68d75cbae07eff905
waltdarby2010 <75259490+waltdarby2010@users.noreply.github.com>
2023-04-17 14:23:01 -0400

    rt

--- main.tf
+++ main.tf
@@ -23,8 +23,6 @@ module "vpc" {
   azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
   
-  enable_nat_gateway = true
-
   tags = {
     Terraform = "true"
     Environment = "dev"
@@ -34,7 +32,7 @@ module "vpc" {
 resource "aws_instance" "blog" {
   ami                    = data.aws_ami.app_ami.id
   instance_type          = var.instance_type
-  
+
   vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
