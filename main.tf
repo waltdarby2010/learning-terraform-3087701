ab4ec8d486ca69c5ce179f0a052b0867e080702b
waltdarby2010 <75259490+waltdarby2010@users.noreply.github.com>
2023-04-17 14:32:52 -0400

    fix me please

--- main.tf
+++ main.tf
@@ -14,25 +14,13 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
-
-  name = "dev"
-  cidr = "10.0.0.0/16"
-
-  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-  
-  tags = {
-    Terraform = "true"
-    Environment = "dev"
-  }
+data "aws_vpc" "default" {
+  default = true
 }
 
 resource "aws_instance" "blog" {
   ami                    = data.aws_ami.app_ami.id
   instance_type          = var.instance_type
-
   vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
@@ -42,10 +30,10 @@ resource "aws_instance" "blog" {
 
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
-  version = "4.13.0"
+  version = "4.17.2"
   name = "blog"
 
-  vpc_id  = module.vpc.public_subnets[0]
+  vpc_id  = data.aws_vpc.default.id
   
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
