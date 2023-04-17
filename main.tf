4fccd858221639cc85e0ae765eef66f3a043f84d
waltdarby2010 <75259490+waltdarby2010@users.noreply.github.com>
2023-04-17 14:02:38 -0400

    2nd try

--- main.tf
+++ main.tf
@@ -23,6 +23,8 @@ module "vpc" {
   azs             = ["us-west-2a", "us-west-1b", "us-west-1c"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
+  enable_nat_gateway = true
+
   tags = {
     Terraform = "true"
     Environment = "dev"
@@ -32,7 +34,7 @@ module "vpc" {
 resource "aws_instance" "blog" {
   ami                    = data.aws_ami.app_ami.id
   instance_type          = var.instance_type
-  subnet_id              = module.blog_vpc.public_subnets[0]
+  
   vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
@@ -44,8 +46,9 @@ module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.13.0"
 
-  vpc_id  = module.vpc.public_subnets[0]
   name    = "blog"
+  vpc_id  = module.vpc.public_subnets[0]
+
   ingress_rules = ["https-443-tcp","http-80-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
   egress_rules = ["all-all"]
