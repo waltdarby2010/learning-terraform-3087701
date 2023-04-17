4884495c093ca3d9319433e80226b11a08b86b3f
waltdarby2010 <75259490+waltdarby2010@users.noreply.github.com>
2023-04-17 14:07:05 -0400

    no nat no

--- main.tf
+++ main.tf
@@ -22,8 +22,6 @@ module "vpc" {
 
   azs             = ["us-west-1a", "us-west-1b", "us-west-1c"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-
-  enable_nat_gateway = true
   
   tags = {
     Terraform = "true"
