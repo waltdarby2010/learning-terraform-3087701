dfbaa46d43589a59731c1549f44141fb84c6652e
waltdarby2010 <75259490+waltdarby2010@users.noreply.github.com>
2023-04-19 13:32:13 -0400

    load balance

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ data "aws_ami" "app_ami" {
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "dev"
+  name = "blog_vpc"
   cidr = "10.0.0.0/16"
 
   azs             = ["us-west-2a", "us-west-1b", "us-west-1c"]
@@ -39,6 +39,49 @@ resource "aws_instance" "blog" {
 
   tags = {
     Name = "Learning Terraform"
+  }
+}
+
+module "alb" {
+  source  = "terraform-aws-modules/alb/aws"
+  version = "~> 8.0"
+
+  name = "blog-alb"
+
+  load_balancer_type = "application"
+
+  vpc_id             = module.blog_vpc.vpc_id
+  subnets            = module.blog_vpc.public_subnets
+  security_groups    = module.blog_sg.security_group_id
+
+
+  target_groups = [
+    {
+      name_prefix      = "blog-"
+      backend_protocol = "HTTP"
+      backend_port     = 80
+      target_type      = "instance"
+      targets = {
+        my_target = {
+          target_id = aws_instance.blog.id
+          port = 80
+        }
+
+      }
+    }
+  ]
+
+
+  http_tcp_listeners = [
+    {
+      port               = 80
+      protocol           = "HTTP"
+      target_group_index = 0
+    }
+  ]
+
+  tags = {
+    Environment = "dev"
   }
 }
 
